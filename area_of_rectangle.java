e136ba3f9a95f43ba69d3d3063ee43b373b0ad11
shanipandeysdr <119961489+shanipandeysdr@users.noreply.github.com>
2023-04-08 23:23:52 +0530

    Create area_of_rectangle.java

---
+++ area_of_rectangle.java
@@ -0,0 +1,13 @@
+
+public class area_of_a_rectangle {
+    public static void main(String[] args) {
+     
+        int length = 6;
+        int breadth = 5;
+        System.out.println("The Length is :" + length);
+        System.out.println("The Breadth is :" + breadth);
+        int area = length * breadth;
+        System.out.print("The Area is: " + area);
+
+    }
+}
